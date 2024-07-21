ead2266e37436237b58a37d1c5bcb2591fee4045
Anirban <abera2583@gmail.com>
2024-07-21 14:43:15 +0530

    adding second commit

--- venv/Library/bin/c_rehash.pl
+++ venv/Library/bin/c_rehash.pl
@@ -13,7 +13,7 @@
 # and add symbolic links to their hash values.
 
 my $dir = "C:\\Program Files\\Common Files\\ssl";
-my $prefix = "D:/diamond price/venv\\Library";
+my $prefix = "d:/diamond price/venv\\Library";
 
 my $errorcount = 0;
 my $openssl = $ENV{OPENSSL} || "openssl";
